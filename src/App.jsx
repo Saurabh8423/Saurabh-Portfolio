36c6488ed8f192c5673f7d3f32a52acbe25f5332
saurabh8423 <saurabhch9369@gmail.com>
2025-10-06 14:46:53 +0530

    Created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Navbar from "./components/Navbar/Navbar"
-
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  
-
   return (
     <>
      <Navbar />
+     <Home />
+     <About />
     </>
   )
 }
