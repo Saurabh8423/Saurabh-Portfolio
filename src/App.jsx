be01d76059678b8096f0b1794917da4e21b7cbe2
saurabh8423 <saurabhch9369@gmail.com>
2025-10-07 19:58:48 +0530

    created contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Project from "./components/Projects/Project";
@@ -10,6 +11,7 @@ function App() {
      <Home />
      <About />
      <Project />
+     <Contact />
     </>
   )
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import con from "../../assets/contact.png";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div id="contact">
+      <div className="left-contact">
+        <img src={con} alt="" />
+      </div>
 
-export default Contact
+      <div className="right-contact">
+        <form action="https://formspree.io/f/xblzalan" method="POST">
+          <input name="Username" type="text" placeholder="Name" />
+          <input name="Email" type="email" placeholder="Email" />
+          <textarea name="message" id="textarea" placeholder="Message me" />
+          <input type="submit" id="btn" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
