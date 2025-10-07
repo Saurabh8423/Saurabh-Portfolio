dd5734f2190f198438a9813d2510364e3c27ae82
saurabh8423 <saurabhch9369@gmail.com>
2025-10-07 14:09:27 +0530

    Created Project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import Project from "./components/Projects/Project";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
      <Navbar />
      <Home />
      <About />
+     <Project />
     </>
   )
 }
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Card from '../Card/Card';
+import "./Project.css"
+import lms from "../../assets/lms.webp"
+import glx from "../../assets/glx.png"
+import nomophobia from "../../assets/Nomophobia.webp"
+import workouts from "../../assets/workouts.png"
+import notes from "../../assets/notes.png"
+import game from "../../assets/game.png"
+
 
 const Project = () => {
   return (
-    <div>
+    <div id='projects'>
+      <h1 id='project-heading'>2+ YEARS EXPERIENCED IN PROJECTS</h1>
+      <div className='slider'>
+        <Card title="LEARNING MANAGEMENT SYSTEM" image={lms}/>
+        <Card title="GALAXY TRAVELS" image={glx}/>
+        <Card title="SMARTPHONE ADDICTION CARE" image={nomophobia}/>
+        <Card title="WORKOUTS" image={workouts}/>
+        <Card title="NOTES APP" image={notes}/>
+        <Card title="ROCK PAPER SCISSORS GAME" image={game}/>
+      </div>
       
     </div>
   )
