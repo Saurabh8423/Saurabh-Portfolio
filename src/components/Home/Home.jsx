3d3b2a549e32a230c5e100f177f2d288a5a52b48
saurabh8423 <saurabhch9369@gmail.com>
2025-10-08 13:11:00 +0530

    Updated Home TypeWriter

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,37 +1,35 @@
-import React from 'react';
-import "./Home.css"
+import React from "react";
+import "./Home.css";
 import man from "../../assets/man.png";
-import TypingEffect from "react-typing-effect";
+import { Typewriter } from "react-simple-typewriter";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-
 const Home = () => {
-  useGSAP(()=>{
-    let tl1=gsap.timeline();
+  useGSAP(() => {
+    let tl1 = gsap.timeline();
     tl1.from(".line1", {
-      y:80,
-      duration:1,
-      opacity:0
-    })
+      y: 80,
+      duration: 1,
+      opacity: 0,
+    });
     tl1.from(".line2", {
-      y:80,
-      duration:1,
-      opacity:0
-    })
+      y: 80,
+      duration: 1,
+      opacity: 0,
+    });
     tl1.from(".line3", {
-      y:80,
-      duration:1,
-      opacity:0
-    })
+      y: 80,
+      duration: 1,
+      opacity: 0,
+    });
 
     gsap.from(".right-home img", {
-      x:200,
-      duration:1,
-      opacity:0
-    })
-  })
-
+      x: 200,
+      duration: 1,
+      opacity: 0,
+    });
+  });
 
   return (
     <div id="home">
@@ -40,26 +38,25 @@ const Home = () => {
           <div className="line1">I'M</div>
           <div className="line2">SAURABH KUMAR</div>
           <div className="line3">
-            {/* <TypingEffect
-              text={["MERN DEVELOPER", "SOFTWARE DEVELOPER","ML & AI"]}
-              speed={500}
-              eraseSpeed={500}
-              eraseDelay={500}
-              cursor='|'
-             /> */}
+            <Typewriter
+              words={["MERN DEVELOPER", "SOFTWARE DEVELOPER", "ML & AI"]}
+              loop={true}
+              cursor
+              cursorStyle="|"
+              typeSpeed={80}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
           </div>
           <button>HIRE ME</button>
         </div>
-
       </div>
 
       <div className="right-home">
-        <img src={man}alt="" />
-
+        <img src={man} alt="" />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default Home;
